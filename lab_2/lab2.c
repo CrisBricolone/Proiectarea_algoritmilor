c3d6d5e5da59f34a9d0317b0402d10b1c3836ee4
Cristi <176015294+CrisBricolone@users.noreply.github.com>
2026-03-03 10:33:53 +0200

    Refactoring the function inside of lab_2

--- lab_2/lab2.c
+++ lab_2/lab2.c
@@ -1,18 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
+int function(int x, int *cnt){
 	x=x-2;
+	(*cnt)++;
+
+	if(x)
+		x = function(x, cnt);
+		
 	return x;
 }
 
 int main(){
 	int a=30, i=0;
 	
-	while (a>0){
-		a=function(a);
-		i++;
-	}
+	a=function(a, &i);
 	printf("%d", i);
 	
 	return 0;	
